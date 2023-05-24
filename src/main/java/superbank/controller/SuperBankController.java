45fa31df2b894a7b0a68b5c36b464f66febf2a91
franciellyl <franciellyelima@gmail.com>
2023-05-24 17:28:07 -0300

    WIP: created client and account class.

--- src/main/java/superbank/controller/SuperBankController.java
+++ src/main/java/superbank/controller/SuperBankController.java
@@ -1,9 +1,19 @@
 package superbank.controller;
 
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping(path = "/super-bank/v1/")
 @RestController
 public class SuperBankController {
+
+    @GetMapping("/healthcheck")
+    public ResponseEntity<String> healthCheck(){
+        return ResponseEntity.ok("UP");
+    }
+
+
 }
--- src/main/java/superbank/service/AccountProcessorService.java
+++ src/main/java/superbank/service/AccountProcessorService.java
@@ -1,4 +1,7 @@
 package superbank.service;
 
+import superbank.model.Client;
+
 public interface AccountProcessorService {
+    void newClient(Client client);
 }
