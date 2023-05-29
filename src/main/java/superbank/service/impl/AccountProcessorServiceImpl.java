a49e133151e039ccef0c745627cc58cb5d63605d
franciellyl <franciellyelima@gmail.com>
2023-05-29 20:01:15 -0300

    WIP: Add new-client endpoint.

--- src/main/java/superbank/service/impl/AccountProcessorServiceImpl.java
+++ src/main/java/superbank/service/impl/AccountProcessorServiceImpl.java
@@ -1,12 +1,14 @@
 package superbank.service.impl;
 
+import org.springframework.stereotype.Service;
 import superbank.model.Client;
 import superbank.service.AccountProcessorService;
 
+@Service
 public class AccountProcessorServiceImpl implements AccountProcessorService {
 
     @Override
     public void newClient(Client client) {
-
+        //In progress
     }
 }
